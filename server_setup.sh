0c737448970c0b44612aad441c772b105cf257d8
Kieran <kieran@onionland.local>
2022-07-13 22:28:38 +0100

    Change volume target

--- server_setup.sh
+++ server_setup.sh
@@ -3,16 +3,15 @@ echo '* libraries/restart-without-asking boolean true' | sudo debconf-set-select
 apt-get update
 apt-get upgrade -y
 apt-get install -y docker.io s3fs awscli
-
+echo "user_allow_other" >> /etc/fuse.conf
 mkdir /db
-
-echo "s3fs#kieran-smith-rightmove-db /db fuse _netdev,iam_role=rightmove_webserver" >> /etc/fstab
+echo "s3fs#kieran-smith-rightmove-db /db fuse _netdev,iam_role=rightmove_webserver,allow_other" >> /etc/fstab
 mount -a
 
 chmod 666 /db/*
 sudo -u ubuntu -i <<'EOF'
 aws s3 sync s3://kieran-smith-rightmove-code ~
 sudo docker build . -t rightmove
-sudo docker run -p 80:80 -d -v $(pwd)/db:/db rightmove
+sudo docker run -p 80:80 -d -v /db:/db rightmove
 EOF
 
