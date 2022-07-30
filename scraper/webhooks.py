02d7e6fc659e0f9b7cba1560b938a5a8a9be2537
kieran-smith77 <6058542+kieran-smith77@users.noreply.github.com>
2022-07-30 15:35:44 +0100

    create ssm params

--- scraper/webhooks.py
+++ scraper/webhooks.py
@@ -3,7 +3,7 @@ import boto3
 
 def alert(count):
     ssm = boto3.client('ssm')
-    parameter = ssm.get_parameter(Name='rightmove/scraper/webhooks')
+    parameter = ssm.get_parameter(Name='/rightmove/scraper/webhooks')
     webhooks = parameter['Parameter']['Value'].split(',')
     for webhook in webhooks:
         requests.post(webhook, json={"value1": str(count)})
