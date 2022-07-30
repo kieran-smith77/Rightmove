2c1bc9c1be5be04919f6a9eed8376e753c535331
kieran-smith77 <6058542+kieran-smith77@users.noreply.github.com>
2022-07-30 18:22:23 +0100

    restart docker

--- server_setup.sh
+++ server_setup.sh
@@ -6,5 +6,5 @@ apt-get install -y docker.io awscli
 sudo -u ubuntu -i <<'EOF'
 aws s3 sync s3://kieran-smith-rightmove-code ~
 sudo docker build . -t rightmove
-sudo docker run -p 80:80 -d rightmove
+sudo docker run -p 80:80 -d --restart unless-stopped rightmove
 EOF
